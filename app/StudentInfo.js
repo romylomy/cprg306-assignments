b42ea800b59748537f2c768dddfa6d1e3f47cda6
Jerome Corpuz <jerome.corpuz@edu.sait.ca>
2023-09-18 02:19:55 -0600

    styling and routing finish with hydration issues

---
+++ app/StudentInfo.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { FaGithubAlt } from 'react-icons/fa';
+import { frame } from 'framer-motion';
+
+const infos = [
+  {
+    Name: 'Jerome Corpuz',
+    Section: 'CPRG 306 C',
+    Github: 'https://github.com/romylomy'
+  }
+];
+
+export default function StudentInfo() {
+  return (
+    <div className='p-2 text-slate-700 rounded-lg'>
+      {infos.map((info, index) => (
+        <div className='w-3/4 'key={index}>
+          {Object.keys(info).map((key) => // map through each attribute and display attribute name with corresponding value
+            key === 'Github' ? ( // if attribute is Github render icon and a tag 
+              <div key={key} className='flex gap-2 items-center	'>
+                < FaGithubAlt/>
+                <a href={info[key]}>{info[key]}</a>
+              </div>
+            ) : (
+              <div key={key} className='flex gap-2 '> 
+                <p className='font-bold'>{`${key}:`}</p>
+                <p>{info[key]}</p> 
+              </div> 
+            )
+          )}
+        </div>
+      ))}
+
+    </div>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,53 @@
+'use client'
+import StudentInfo from './StudentInfo'
+import Link from 'next/link'
+import { motion} from 'framer-motion'
+
+const links = [
+  {
+    folder:'week2',
+    name:'Week 2'
+  },
+  {
+    folder:'week3',
+    name:'Week 3'
+  }
+]
+
+
+export default function Home() {
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+          <div 
+            className='font-libre text-sm '>
+
+            <motion.h1 
+             initial={{  opacity: 0, scale: 0.5 }}
+             animate={{  opacity: 1, scale: 1 }}
+
+            className='text-balance text-lime-800 text-4xl font-bold mb-5 '>
+              CPRG 306:<br/>Web Development 2 <br/> -Assignments
+            </motion.h1>
+
+            <motion.div
+                initial={{ opacity: 0}}
+                animate={{ opacity: 1}}
+                transition={{ duration: 0.9 }}
+                className='flex flex-col gap-7 column-1 '>
+
+                  <StudentInfo/>
+                  <div className='flex flex-col gap-1 column-1 '>
+                      { links.map(link=>(
+                          <Link href={`/${link.folder}`} className=' text-center w-3/4 text-xs py-1 font-bold px-2 border-2 border-slate-700 rounded-md hover:bg-emerald-600 active:bg-slate-700 focus:outline-none focus:ring focus:ring-slate-600 '> {link.name} </Link>
+
+                      ))}
+                  </div>
+
+            </motion.div>
+           
+            
+
+          </div>
+    </main>
+  )
+}
