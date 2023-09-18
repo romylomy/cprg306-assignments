2df84b290d6f4119a56e481d77e06dc7dae6a882
Jerome Corpuz <jerome.corpuz@edu.sait.ca>
2023-09-18 12:45:16 -0600

    added lottie animation

--- app/page.js
+++ app/page.js
@@ -2,6 +2,9 @@
 import StudentInfo from './StudentInfo'
 import Link from 'next/link'
 import { motion} from 'framer-motion'
+import Lottie from 'lottie-react'
+import animationData from '@/app/animationData.json'
+
 
 const links = [
   {
@@ -15,20 +18,20 @@ const links = [
 ]
 
 const linkElement =  links.map(link=>(
-  <Link href={`/${link.folder}`} className=' text-center w-2/4 text-xs py-1 font-bold px-2 border-2 border-slate-700 rounded-md hover:bg-emerald-600 active:bg-slate-700 focus:outline-none focus:ring focus:ring-slate-600 '> 
+  <Link href={`/${link.folder}`} className=' text-center w-2/4 text-xs py-1  px-2 border-2 border-slate-700 rounded-md hover:bg-emerald-600 active:bg-slate-700 focus:outline-none focus:ring focus:ring-slate-600 '> 
   {link.name} </Link>
 
 ))
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col justify-between p-24'>
+    <main className='flex min-h-screen justify-around align-center p-24 '>
           <div 
             className='font-libre text-sm '>
 
             <motion.h1 
-             initial={{  opacity: 0, scale: 0.5 }}
-             animate={{  opacity: 1, scale: 1 }}
+             initial={{  xl:-300,opacity: 0, scale: 0.5 }}
+             animate={{  x:0, opacity: 1, scale: 1 }}
             className='text-balance text-lime-800 text-4xl font-bold mb-5 '>
 
             CPRG 306:<br/>Web Development 2 <br/> -Assignments
@@ -50,6 +53,9 @@ export default function Home() {
            
             
 
+          </div>
+          <div className=' md:w-1/2 lg:w-1/2 xl:w-1/2' >
+              <Lottie loop={false}  animationData={animationData}/>
           </div>
     </main>
   )
