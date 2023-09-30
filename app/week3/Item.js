f0fd190a41745d304e22cad213305b3b958876fe
Jerome Corpuz <jerome.corpuz@edu.sait.ca>
2023-09-30 10:11:09 -0600

    adding colors to each category name

--- app/week3/Item.js
+++ app/week3/Item.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Item({ name, icon, quantity }) {
   return (
-    <div className=" font-mono shadow-lg rounded-lg text-slate-700 bg-slate-200 bg-opacity-50">
+    <div className="font-mono shadow-lg rounded-lg text-slate-700 bg-slate-100 bg-opacity-50">
         <div className="p-4">
             <p className="text-2xl">{icon}</p>
             <h2 className="text-xl">{name}</h2>
