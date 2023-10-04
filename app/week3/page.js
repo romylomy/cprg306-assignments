5bf9adf59c5dc4049e4c2281014726c68a46a158
Jerome Corpuz <jerome.corpuz@edu.sait.ca>
2023-10-04 16:55:17 -0600

    fixing vercel deloymeny isssue

--- app/week3/page.js
+++ app/week3/page.js
@@ -6,17 +6,17 @@ import Item from './Item'; // Update the import for 'Item' with the correct file
 export default function Page() { // Use PascalCase for the component name.
 
   return (
-    <section className='p-5 md:px-10 lg:px-60'>
-      <div className="p-10 shadow-lg bg-gray-600 bg-opacity-50 rounded-xl">
-        <div className=' p-4 rounded-lg my-10 flex align-center justify-center  '>
-             <p className='font-raleway text-3xl lg:text-5xl py-4 text-bold'>{`${ItemList.length} items`}</p>
+    <section className=' bg-slate-100 p-5 md:px-10 lg:px-60'>
+      <div className="p-10  ">
+        <div className=' p-4 shadow-lg rounded-lg my-10 flex align-center justify-center  '>
+             <p className='font-bebas text-3xl lg:text-5xl py-4 text-bold'>{`${ItemList.length} items`}</p>
               <HiOutlineClipboardList size={70}/>
 
         </div>
 
-        <div className='grid gap-2 grid-cols-1 md:grid-cols-2'>
+        <div className='grid gap- grid-cols-1 md:grid-cols-2'>
 
-          <div className='  bg-opacity-10 rounded-lg p-5 '>
+          <div className=' shadow-lg border-2 border-slate-200 bg-opacity-10 rounded-lg p-5 '>
             <div className='mb-5 text-center rounded-lg bg-opacity-50 bg-purple-300 '>
               <h1 className='font-lora text-bold text-purple-700 font-ubuntu  text-3xl'>Household</h1>
             </div>
@@ -33,7 +33,7 @@ export default function Page() { // Use PascalCase for the component name.
             </div>
           </div>
 
-          <div className='bg-opacity-10 rounded-lg p-5'>
+          <div className='shadow-lg border-2 border-slate-200 bg-opacity-10 rounded-lg p-5'>
           <div className='mb-5 text-center rounded-lg bg-opacity-50 bg-green-300 '>
               <h1 className='font-lora text-bold text-green-700 font-ubuntu  text-3xl'>Produce</h1>
             </div>
@@ -49,7 +49,7 @@ export default function Page() { // Use PascalCase for the component name.
             </div>
           </div>
 
-          <div className='bg-opacity-10 rounded-lg p-5'>
+          <div className='shadow-lg border-2 border-slate-200 bg-opacity-10 rounded-lg p-5'>
             <div className='mb-5 text-center rounded-lg bg-opacity-50 bg-orange-300 '>
                 <h1 className='font-lora text-bold text-orange-700 font-ubuntu  text-3xl'>Meat</h1>
               </div>
@@ -65,7 +65,7 @@ export default function Page() { // Use PascalCase for the component name.
             </div>
           </div>
 
-          <div className='bg-opacity-10 rounded-lg p-5'>
+          <div className='shadow-lg border-2 border-slate-200 bg-opacity-10 rounded-lg p-5'>
             <div className='mb-5 text-center rounded-lg bg-opacity-50 bg-yellow-300 '>
                   <h1 className='font-lora text-bold text-yellow-700 font-ubuntu  text-3xl'>Canned Goods</h1>
                 </div>
@@ -81,20 +81,20 @@ export default function Page() { // Use PascalCase for the component name.
             </div>
           </div>
 
-          <div className='bg-opacity-10 rounded-lg p-5'>
-          <div className='mb-5 text-center rounded-lg bg-opacity-50 bg-blue-300 '>
-                <h1 className='font-lora text-bold text-blue-700 font-ubuntu  text-3xl'>Dry Goods</h1>
+          <div className='shadow-lg border-2 border-slate-200 bg-opacity-10 rounded-lg p-5'>
+            <div className='mb-5 text-center rounded-lg bg-opacity-50 bg-blue-300 '>
+                  <h1 className='font-lora text-bold text-blue-700 font-ubuntu  text-3xl'>Dry Goods</h1>
+                </div>
+              <div className='grid grid-cols-1 gap-4'>
+                {ItemList.map((item, index) => {
+                  if (item.category === 'dry goods') {
+                    return (
+                      <Item key={index} {...item} />
+                    );
+                  }
+                  return null;
+                })}
               </div>
-            <div className='grid grid-cols-1 gap-4'>
-              {ItemList.map((item, index) => {
-                if (item.category === 'dry goods') {
-                  return (
-                    <Item key={index} {...item} />
-                  );
-                }
-                return null;
-              })}
-            </div>
           </div>
 
 
