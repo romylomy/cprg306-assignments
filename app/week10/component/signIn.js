56efc04237bfa42f2b876431a60d0a8f62f3a097
Jerome Corpuz <jerome.corpuz@edu.sait.ca>
2023-12-02 21:36:09 -0700

    styled sign in/create acc page

---
+++ app/week10/component/signIn.js
@@ -0,0 +1,107 @@
+'use client'
+import Link from 'next/link'
+import { motion} from 'framer-motion'
+import Lottie from 'lottie-react'
+import animationData from 'app/animationData.json'
+
+
+const links = [
+  {
+    folder:'week3',
+    name:'Week 3'
+  },
+  {
+    folder:'week4',
+    name:'Week 4'
+  },
+  {
+    folder:'week5',
+    name:'Week 5'
+  },
+  {
+    folder:'week6',
+    name:'Week 6'
+  },
+  {
+    folder:'week7',
+    name:'Week 7'
+  },
+  {
+    folder:'week8',
+    name:'Week 8'
+  },
+  {
+    folder:'week10',
+    name:'Week 10'
+  }
+]
+
+const linkElement =  links.map(link=>(
+  <Link href={`/${link.folder}`} className=' text-center w-1/2 z-[2] p-5 shadow-xl text-sm lg:text-lg py-1 bg-slate-700 bg-opacity-80  px-2 border-2 border-slate-700 border-opacity-50 rounded-md  hover:bg-slate-300 active:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-700 '> 
+  {link.name} </Link>
+
+))
+
+export default function signIn() {
+  return (
+    <main className=' bg-slate-50 lg:bg-slate-50 text-slate-50 flex lg:flex-cols items-center justify-center h-screen  '>
+  
+
+      
+        
+      
+        <div className='grid grid-cols-1 justify-between w-full '>
+          <div>
+
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 1.0 }}>
+
+              <Lottie className='shadow-inner bg-slate-300 px-30 lg:px-80' animationData={animationData}/>
+ 
+
+            </motion.div>
+              
+             
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.9 }}
+              className='mx-10 md:mx-20 '>
+              <div className=" py-5   grid lg:flex lg:justify-around lg:items-center   ">
+                  <h1
+                      className='lg:text-center font-OpenSans rounded-lg  text-blue-700 lg:bg-none  text-balance  bg-opacity-70  shadow-orange-700  text-4xl font-bold mb-5 lg:text-5xl'>
+              
+                      CPRG 306:<br />
+              
+                </h1>
+                <h1
+                      className='lg:text-center font-OpenSans rounded-lg  text-blue-800 lg:bg-none  text-balance  bg-opacity-70  shadow-orange-700  text-3xl font-bold mb-5 lg:text-4xl'>
+              
+                       Web Development 2 <br/> Assignments
+              
+                </h1>
+                
+              </div>
+              <div className=' grid grid-cols-1 items-center gap-1 lg:flex lg:justify-between '>
+                  
+                  {linkElement}
+              </div>
+          </motion.div>
+          
+              
+      
+
+          </div>
+          
+          
+          
+        </div>
+
+     
+    
+  
+    </main>
+  )
+  }  
--- app/week10/page.js
+++ app/week10/page.js
@@ -6,6 +6,12 @@ import { FaClipboardList } from "react-icons/fa6";
 import { IoLogOut } from "react-icons/io5";
 import { FaGithub, FaGoogle } from "react-icons/fa";
 import { MdPlaylistAddCheckCircle } from "react-icons/md";
+import {motion} from "framer-motion"
+import MealMingle from 'public/MealMingle.json'
+import Lottie from 'lottie-react'
+import { TypeAnimation } from 'react-type-animation';
+
+
 
 
 const Page = () => {
@@ -45,27 +51,96 @@ const Page = () => {
           </div>
         </div>
       ) : (
-        <div className="min-h-screen flex flex-cols items-center justify-center relative ">
-          <div className="grid grid-cols-1 gap-y-5 items-center">
-            <h1 className="sm:text-2xl">Please log in to access the content.</h1>
-            <div className="flex justify-center items-center gap-x-2 ">
-              <button className="w-1/2 flex items-center border-2 border-slate-700 justify-around shadow-lg rounded-lg px-3 py-2 hover:bg-orange-400" onClick={handleLogin}>
-                <FaGithub size={30}/> 
-              </button>
-              <button className="w-1/2 flex items-center border-2 border-slate-700 justify-around shadow-lg rounded-lg px-3 py-2 hover:bg-orange-400" onClick={handleLogin2}>
-                <FaGoogle size={30}/> 
-              </button>
-            </div>
-            <div>
-              {/* Image component is not properly closed, fixing it below */}
-              <Image 
-                src="https://www.canva.com/design/DAF1WIJYhpY/view" 
-                alt="Past" // Add alt text for accessibility
-                height={50} 
-                width={50} />
-            </div>
+        <main className=' bg-gray-300 text-slate-50 flex lg:flex-cols items-center justify-center h-screen  '>
+  
+
+      
+        
+      
+        <div className= ' relative grid grid-cols-1 justify-between w-full '>
+          <div>
+
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 1.0 }}
+                >
+
+              <Lottie className=' relative shadow-inner bg-slate-300 px-30 lg:px-80' animationData={MealMingle}>
+
+                <motion.div
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.9 }}
+                  className=' z-[100]  bg-gray-100 bg-opacity-80 rounded-lg absolute bottom-1/2 right-1/2 translate-x-1/2 translate-y-1/2 justify-center items-center border-2 border-slate-700 p-5 '>
+                  <div className=" w-full  p-5 sm:p-15  grid lg:grid lg:grid-cols-1   ">
+                      
+                      
+                        
+                      
+                  <div className="grid grid-cols-1 ">
+
+                        <h1 className="text-4xl text-slate-700">MealMingle</h1>
+                        <TypeAnimation
+                        sequence={[
+                          // Same substring at the start will only be typed out once, initially
+                          
+                          'Add item',
+                          1000,
+                          'find recipe',
+                          1000,
+                          'Cook',
+                          1000,
+                          
+                        
+                        ]}
+                        wrapper="span"
+                        speed={50}
+                        style={{ fontSize: '1em', display: 'inline-block' }}
+                        className=' text-2xl font-bold  bg-clip-text text-transparent bg-gradient-to-r from-red-700 to-orange-500'
+                        repeat={Infinity}
+                        />
+
+                  
+                        <h1 className="mt-4 line-height-[4px] text-md text-slate-700 ">Create account / login </h1>
+
+                              <div className=" flex justify-center items-center gap-y-1 gap-x-2 ">
+
+                                <button className="w-1/2 flex items-center bg-slate-500 border-2 border-slate-700 justify-around shadow-lg rounded-full px-3 py-2 hover:bg-red-500" onClick={handleLogin}>
+                                  <FaGithub size={30}/> 
+                                </button>
+                                <button className="w-1/2 flex items-center  bg-slate-500 border-2 border-slate-700 justify-around shadow-lg rounded-full px-3 py-2 hover:bg-red-500" onClick={handleLogin2}>
+                                  <FaGoogle size={30}/> 
+                                </button>
+                              </div>
+                            </div>
+                  
+                        
+                      </div>
+                      
+                    </motion.div>
+
+              </Lottie>
+ 
+
+            </motion.div>
+              
+             
+          
+              
+      
+
           </div>
+          
+          
+          
         </div>
+
+     
+    
+  
+    </main>
+        
       )}
     </div>
   );
